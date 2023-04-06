b09ca4b988de80dd106d374ea8fe93e776b0fabc
cmarius46 <cmarius46@gmail.com>
2023-04-06 19:13:50 +0300

    removed scroll

--- src/autoBot/action_emulator.py
+++ src/autoBot/action_emulator.py
@@ -1,9 +1,10 @@
 # emulates actions from a certain flow
 
 
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 import logging
 import time
@@ -22,18 +23,26 @@ def emulate_all_flows():
 
 def emulate_one_flow(flow):
 	driver = get_driver_for_browser_ip()
-	
-	time.sleep(1)
+
+	chain = ActionChains(driver)
 
 	# css_selector = 'div.col-sm-3'
-	css_selector = 'layout_padding'
-	try:
-		element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
+	# xpath = "/html[1]/body[1]/div[1]/div[2]/div[1]/sec[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/img[1]"
+	# xpath = "/html[1]/body[1]/div[7]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/for[1]/div[4]/tex[1]"
+	# xpath = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/img[1]'
+	# xpath = "/html[1]/body[1]/div[2]"
+	# /html[1]/body[1]/div[7]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/for[1]/div[4]/tex[1]
+	# /html[1]/body[1]/div[7]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/for[1]/div[4]/tex[1]
 
-		element.click()
-		logging.info('MANUAL click')
-	except:
-		logging.error('Error when manual clicking')
+
+	# try:
+	# 	element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, xpath)))
+
+	# 	element.click()
+	# 	logging.info('MANUAL click')
+	# except Exception as e:
+	# 	logging.error('Error when manual clicking')
+	# 	print(e)
 
 	for action in flow:
 		action_type = _get_action_type(action)
@@ -41,11 +50,11 @@ def emulate_one_flow(flow):
 	
 		match action_type:
 			case 'click':
-				_emulate_click(driver, action)
+				_emulate_click(chain, driver, action)
 			case 'scroll':
-				_emulate_scroll(driver, action)
+				_emulate_scroll(chain, driver, action)
 			case 'input':
-				_emulate_input(driver, action)
+				_emulate_input(chain, driver, action)
 			# case 'DOMContentLoaded':
 			# 	pass
 			# 	print('reaches here')
@@ -55,65 +64,70 @@ def emulate_one_flow(flow):
 			# 	pass
 			# 	_emulate_resize(driver, action)
 
+	time.sleep(1)
 	driver.quit()
 
 
-def _emulate_click(driver, action):
+def _emulate_click(chain, driver, action):
 	try:
-		element = WebDriverWait(driver, 10).until(
-			_get_element_identifier_for_click(action)
-		)
+		# element = WebDriverWait(driver, 2).until(
+		# 	_get_element_identifier_for_click(action)
+		# )
 
-		element.click()
+		element = driver.find_element_by_xpath(action['xpath'])
+
+		chain.move_to_element(element).click().perform()
 		logging.info(f'Clicked element.')
 
 	except Exception as e:
-		logging.error(f'Exception when clicking.')
+		logging.error(f'Exception when clicking.: ' + str(e))
 		pass
 	# else:
 	finally:
 		logging.info('--------------------------------')
 
 
-def _emulate_scroll(driver, action):
+def _emulate_scroll(chain, driver, action):
+	# TODO add scroll_width
 	try:
 		scroll_height = _get_scroll_height(action)
 
-		script = "window.scrollTo(0, " + str(scroll_height) + ");"
-
-		driver.execute_script(script)
+		driver.execute_script('window.scrollTo(0, arguments[0]);', scroll_height)
 
 		logging.info(f'Executed scroll.')
 
 	except Exception as e:
-		logging.error(f'Exception when scrolling.')
+		logging.error(f'Exception when scrolling.' + str(e))
 		pass
 	# else:
 	finally:
 		logging.info('--------------------------------')
 
 
-def _emulate_input(driver, action):
+def _emulate_input(chain, driver, action):
 	# TODO add behaviour for special cases e.g. "inputType": "deleteContentBackward",
 	try:
-		input_element = WebDriverWait(driver, 10).until(
-			_get_element_identifier_for_input(action)
-		)
+		# input_element = WebDriverWait(driver, 2).until(
+		# 	_get_element_identifier_for_input(action)
+		# )
 
 		keys_to_input = _get_keys_to_input(action)
+		
+		element = driver.find_element_by_xpath(action['xpath'])
+		chain.move_to_element(element).click().send_keys(keys_to_input).perform()
+		# move_to_element(element).click() suplimentar, maybe TOBE removed later
 
-		input_element.send_keys(keys_to_input)
 		logging.info(f'Sent input.')
 
 	except Exception as e:
-		logging.error(f'Exception when inputting.')
+		logging.error(f'Exception when inputting.' + str(e))
 		pass
 	# else:
 	finally:
 		logging.info('--------------------------------')
 
 
-def _emulate_DOMContentLoaded(driver, action):
+def _emulate_DOMContentLoaded(chain, action):
 	try:
 		inner_height, inner_width = _get_window_size_for_DOMContentLoaded(action)
 
@@ -131,7 +145,7 @@ def _emulate_DOMContentLoaded(driver, action):
 		logging.info('--------------------------------')
 
 
-def _emulate_resize(driver, action):
+def _emulate_resize(chain, action):
 	try:
 		inner_height, inner_width = _get_window_size_for_resize(action)
 
@@ -152,15 +166,11 @@ def _emulate_resize(driver, action):
 def _get_element_identifier_for_click(action):
 	# this returns an EC.function corresponding to the looked for element
 
-	element_id = _get_action_id(action)
-	if element_id is not None:
-		return EC.presence_of_element_located((By.ID, element_id))
+	element_xpath = _get_element_xpath(action)
+	if element_xpath is not None:
+		return EC.presence_of_element_located((By.XPATH, element_xpath))
 
-	css_selector = _get_action_css_selector(action)
-	if css_selector is not None:
-		return EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
-
-	logging.error('error fetching object. Object has no id or css_selector')
+	logging.error('Error identifying object. Received "None" from identifier ')
 	return None
 
 
@@ -168,13 +178,18 @@ def _get_element_identifier_for_input(action):
 	# this returns an EC.function corresponding to the looked for element
 	# identical to the one for click. Are separated since they might need particular modifications later on
 
-	element_id = _get_action_id(action)
-	if element_id is not None:
-		return EC.presence_of_element_located((By.ID, element_id))
+	
+	element_xpath = _get_element_xpath(action)
+	if element_xpath is not None:
+		return EC.presence_of_element_located((By.XPATH, element_xpath))
 
-	css_selector = _get_action_css_selector(action)
-	if css_selector is not None:
-		return EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
+	# element_id = _get_action_id(action)
+	# if element_id is not None:
+	# 	return EC.presence_of_element_located((By.ID, element_id))
+
+	# css_selector = _get_action_css_selector(action)
+	# if css_selector is not None:
+	# 	return EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
 
 	logging.error('error fetching object. Object has no id or css_selector')
 	return None
@@ -213,6 +228,14 @@ def _get_window_size_for_resize(action):
 			logging.error('error getting window size for resize : action target has no "innerHeight" or "innerWidth" field.')
 
 	return inner_height, inner_width
+
+
+def _get_element_xpath(element):
+	try:
+		return element['xpath']
+	except:
+		logging.error('error getting element xpath : element has no "xpath" field.')
+	return None
 
 
 def _get_keys_to_input(action):
@@ -259,6 +282,12 @@ def _get_action_css_selector(action):
 
 
 def _get_scroll_height(action):
+	# try:
+	# 	return action['scrollY']
+	# except Exception as e:
+	# 	print('Exception getting the scrollY property'.)
+	# 	return None
+
 	try:
 		action_target = action['target']
 	except Keyerror as ke:
