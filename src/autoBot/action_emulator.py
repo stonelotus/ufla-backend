f8f99d7e6f42c98ed160af81c186e4ac75bebb73
cmarius46 <cmarius46@gmail.com>
2023-04-08 20:56:56 +0300

    working BOT

--- src/autoBot/action_emulator.py
+++ src/autoBot/action_emulator.py
@@ -1,9 +1,6 @@
 # emulates actions from a certain flow
 
 
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -36,18 +33,18 @@ def emulate_one_flow(flow):
 				emulate_click(chain, driver, action)
 			case 'dblclick':
 				emulate_dblclick(chain, driver, action)
-			case 'mousedown':
-				emulate_mousedown(chain, driver, action)
-			case 'mouseup':
-				emulate_mouseup(chain, driver, action)
+			# case 'mousedown':
+			# 	emulate_mousedown(chain, driver, action)
+			# case 'mouseup':
+			# 	emulate_mouseup(chain, driver, action)
 			case 'mousemove':
 				emulate_mousemove(chain, driver, action)
 			case 'mouseover':
 				emulate_mouseover(chain, driver, action)
-			case 'mouseenter':
-				emulate_mouseover(chain, driver, action) # same function as mouseover
-			# case 'mouseout':
-			# 	emulate_mouseout(chain, driver, action)
+			# case 'mouseenter':
+			# 	emulate_mouseover(chain, driver, action) # same function as mouseover
+			case 'mouseout':
+				emulate_mouseout(chain, driver, action)
 			# case 'mouseleave':
 			# 	emulate_mouseout(chain, driver, action) # same function as mouseout
 			# # Commented as they add no value and increase error%
@@ -60,25 +57,33 @@ def emulate_one_flow(flow):
 			case 'resize':
 				emulate_resize(chain, driver, action)
 
-			case 'input':
-				emulate_input(chain, driver, action)
 			case 'DOMContentLoaded':
 				# the purpose of this is to properly resize the window at the beginning of the session
 				emulate_DOMContentLoaded(chain, driver, action)
 
 			# keyboard events
-			case 'keydown':
-				emulate_keydown(chain, driver, action)
-			case 'keyup':
-				emulate_keyup(chain, driver, action)
-			case 'keypress':
-				emulate_keypress(chain, driver, action)
+			# case 'keydown':
+			# 	emulate_keydown(chain, driver, action)
+			# case 'keyup':
+			# 	emulate_keyup(chain, driver, action)
+			# case 'keypress':
+			# 	emulate_keypress(chain, driver, action)
 			
 			# form events
-			case 'submit':
-				emulate_submit(chain, driver, action)
-			
-			
+			# case 'submit':
+			# 	emulate_submit(chain, driver, action)
+			case 'input':
+				emulate_input(chain, driver, action)
+			# case 'blur':
+			# 	emulate_blur(chain, driver, action)
+			# case 'focus':
+			# 	emulate_focus(chain, driver, action)
+			# case 'reset':
+			# 	emulate_reset(chain, driver, action)
+			# case 'change':
+			# 	emulate_change(chain, driver, action)
+			case 'select':
+				emulate_select(chain, driver, action)
 
 	time.sleep(5)
 
@@ -266,6 +271,9 @@ def emulate_contextmenu(chain, driver, action):
 # WINDOW EVENTS
 def emulate_scroll(chain, driver, action):
 	# TODO add scroll_width
+
+	time.sleep(0.1) # make sure there is enough time for scroll
+
 	try:
 		scrollX, scrollY = helpers.get_scroll_params(action)
 
@@ -370,28 +378,28 @@ def emulate_keyup(chain, driver, action):
 
 
 def emulate_keypress(chain, driver, action):
-    try:
+	try:
 		key = helpers.get_key(action)
 		ctrl_key = helpers.get_ctrl_key(action)
-        if key is None:
-            logging.error("Key must be specified for keypress event.")
-            return
+		if key is None:
+			logging.error("Key must be specified for keypress event.")
+			return
 
-        if ctrl_key:
-            chain.key_down(Keys.CONTROL)
+		if ctrl_key:
+			chain.key_down(Keys.CONTROL)
 
-        chain.send_keys(key).perform()
+		chain.send_keys(key).perform()
 
-        if ctrl_key:
-            chain.key_up(Keys.CONTROL)
+		if ctrl_key:
+			chain.key_up(Keys.CONTROL)
 
-        logging.info(f'Performed keypress on {key} with ctrlKey={ctrl_key}.')
+		logging.info(f'Performed keypress on {key} with ctrlKey={ctrl_key}.')
 
-    except Exception as e:
-        logging.error(f'Exception when performing keypress with ctrlKey={ctrl_key}: ' + str(e))
-        pass
-    finally:
-        logging.info('--------------- End of action -----------------')
+	except Exception as e:
+		logging.error(f'Exception when performing keypress with ctrlKey={ctrl_key}: ' + str(e))
+		pass
+	finally:
+		logging.info('--------------- End of action -----------------')
 
 
 # Form events
@@ -422,134 +430,136 @@ def emulate_input(chain, driver, action):
 
 
 def emulate_blur(chain, driver, action):
-    try:
+	try:
 		element = helpers.get_element(driver, action)
-        if element is None:
-            logging.error("Element must be specified for blur event.")
-            return
+		if element is None:
+			logging.error("Element must be specified for blur event.")
+			return
 
-        body = driver.find_element_by_tag_name('body')
-        chain.move_to_element(element).click().move_to_element(body).click().perform()
-        logging.info(f'Blurred the element.')
+		body = driver.find_element_by_tag_name('body')
+		chain.move_to_element(element).click().move_to_element(body).click().perform()
+		logging.info(f'Blurred the element.')
 
-    except Exception as e:
-        logging.error(f'Exception when blurring the element: ' + str(e))
-        pass
-    finally:
-        logging.info('--------------- End of action -----------------')
+	except Exception as e:
+		logging.error(f'Exception when blurring the element: ' + str(e))
+		pass
+	finally:
+		logging.info('--------------- End of action -----------------')
 
 
 def emulate_focus(chain, driver, action):
-    try:
+	try:
 		element = helpers.get_element(driver, action)
-        if element is None:
-            logging.error("Element must be specified for focus event.")
-            return
+		if element is None:
+			logging.error("Element must be specified for focus event.")
+			return
 
-        chain.move_to_element(element).click().perform()
-        logging.info(f'Focused the element.')
+		chain.move_to_element(element).click().perform()
+		logging.info(f'Focused the element.')
 
-    except Exception as e:
-        logging.error(f'Exception when focusing the element: ' + str(e))
-        pass
-    finally:
-        logging.info('--------------- End of action -----------------')
+	except Exception as e:
+		logging.error(f'Exception when focusing the element: ' + str(e))
+		pass
+	finally:
+		logging.info('--------------- End of action -----------------')
 
 
 def emulate_submit(driver, action):
-    try:
+	try:
 		element = helpers.get_element(driver, action)
-        if element is None:
-            logging.error("Element must be specified for submit event.")
-            return
+		if element is None:
+			logging.error("Element must be specified for submit event.")
+			return
 
-        element.submit()
-        logging.info(f'Submitted the form.')
+		element.submit()
+		logging.info(f'Submitted the form.')
 
-    except Exception as e:
-        logging.error(f'Exception when submitting the form: ' + str(e))
-        pass
-    finally:
-        logging.info('--------------- End of action -----------------')
+	except Exception as e:
+		logging.error(f'Exception when submitting the form: ' + str(e))
+		pass
+	finally:
+		logging.info('--------------- End of action -----------------')
 
 
 def emulate_reset(driver, action):
-    try:
+	try:
 		# TODO make sure this gets the right element in this context
 		form_element = helpers.get_element(driver, action)
-        if form_element is None:
-            logging.error("Form element must be specified for reset event.")
-            return
+		if form_element is None:
+			logging.error("Form element must be specified for reset event.")
+			return
 
-        reset_script = 'arguments[0].reset();'
-        driver.execute_script(reset_script, form_element)
-        logging.info(f'Reset the form.')
+		reset_script = 'arguments[0].reset();'
+		driver.execute_script(reset_script, form_element)
+		logging.info(f'Reset the form.')
 
-    except Exception as e:
-        logging.error(f'Exception when resetting the form: ' + str(e))
-        pass
-    finally:
-        logging.info('--------------- End of action -----------------')
+	except Exception as e:
+		logging.error(f'Exception when resetting the form: ' + str(e))
+		pass
+	finally:
+		logging.info('--------------- End of action -----------------')
 
 
 def emulate_change(chain, driver, action):
-    try:
+	try:
 		element = helpers.get_element(driver, action)
 		value = helpers.get_value(action)
-        if element is None:
-            logging.error("Element and value must be specified for change event.")
-            return
+		if element is None:
+			logging.error("Element and value must be specified for change event.")
+			return
 
-        # Check if the element is a checkbox or radio button
-        is_checkbox = element.tag_name == 'input' and element.get_attribute('type') == 'checkbox'
-        is_radio = element.tag_name == 'input' and element.get_attribute('type') == 'radio'
+		# Check if the element is a checkbox or radio button
+		is_checkbox = element.tag_name == 'input' and element.get_attribute('type') == 'checkbox'
+		is_radio = element.tag_name == 'input' and element.get_attribute('type') == 'radio'
 
-        if is_checkbox or is_radio:
-            # Simulate a space key press to toggle the checkbox or radio button
-            chain.move_to_element(element).click().key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
-        else:
-            # Move to the element, click it, and send the new value
-            chain.move_to_element(element).click().send_keys(value).perform()
+		if is_checkbox or is_radio:
+			# Simulate a space key press to toggle the checkbox or radio button
+			chain.move_to_element(element).click().key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
+		else:
+			# Move to the element, click it, and send the new value
+			chain.move_to_element(element).click().send_keys(value).perform()
 
-        # Trigger the 'change' event on the element using ActionChains
-        chain.move_to_element(element).click().perform()
+		# Trigger the 'change' event on the element using ActionChains
+		chain.move_to_element(element).click().perform()
 
-        logging.info(f'Changed the value of the element.')
+		logging.info(f'Changed the value of the element.')
 
-    except Exception as e:
-        logging.error(f'Exception when changing the value of the element: ' + str(e))
-        pass
-    finally:
-        logging.info('--------------- End of action -----------------')
+	except Exception as e:
+		logging.error(f'Exception when changing the value of the element: ' + str(e))
+		pass
+	finally:
+		logging.info('--------------- End of action -----------------')
 
 
 def emulate_select(chain, driver, action):
-    try:
+	try:
 		element = helpers.get_element(driver, action)
 		start, end = helpers.get_start_end(action)
 
-        if element is None:
-            logging.error("Element must be specified for select event.")
-            return
+		print(start, end)
 
-        if end is None or end == '':
-            # If end is not specified, select from start to end of text
-            end = len(helpers.get_value(action))
+		if element is None:
+			logging.error("Element must be specified for select event.")
+			return
 
-        # Move to the element and click to focus it
-        chain.move_to_element(element).click().perform()
+		if end is None or end == '':
+			# If end is not specified, select from start to end of text
+			end = len(helpers.get_value(action))
 
-        # Select the text in the element using ActionChains
-        actions = ActionChains(driver)
-        actions.move_to_element(element).click_and_hold().move_by_offset(start, 0).move_by_offset(end - start, 0).release().perform()
+		# Move to the element and click to focus it
+		chain.move_to_element(element).click().perform()
 
-        logging.info(f'Selected text in the element.')
+		# Select the text in the element using ActionChains
+		actions = ActionChains(driver)
+		actions.move_to_element(element).click_and_hold().move_by_offset(start, 0).move_by_offset(end - start, 0).release().perform()
 
-    except Exception as e:
-        logging.error(f'Exception when selecting text in the element: ' + str(e))
-        pass
-    finally:
-        logging.info('--------------- End of action -----------------')
+		logging.info(f'Selected text in the element.')
+
+	except Exception as e:
+		logging.error(f'Exception when selecting text in the element: ' + str(e))
+		pass
+	finally:
+		logging.info('--------------- End of action -----------------')
 
 
 def _handle_insert_text_event(chain, input_element, keys_to_input):
--- src/autoBot/helpers.py
+++ src/autoBot/helpers.py
@@ -1,61 +1,68 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+import logging
+import time
+
 
 def get_element(driver, action):
-	try:
-		element_xpath = _get_element_xpath(action)
+    try:
+        element_xpath = _get_element_xpath(action)
 
-		element = WebDriverWait(driver, 2).until(
-			EC.presence_of_element_located((By.XPATH, element_xpath))
-		)
+        element = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, element_xpath))
+        )
 
-		driver.execute_script("arguments[0].scrollIntoView(true);", element)
+        driver.execute_script("arguments[0].scrollIntoView(true);", element)
 
-		element = WebDriverWait(driver, 10).until(
-			EC.visibility_of_element_located((By.XPATH, element_xpath))
-		)
+        element = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, element_xpath))
+        )
 
-		# should do the same as the above, however the above doesn't perform properly for some reason
-		time.sleep(0.5)
+        # should do the same as the above, however the above doesn't perform properly for some reason
+        time.sleep(0.5)
 
-		return element
+        return element
 
-	except:
-		logging.error(f'Exception when getting element')
+    except:
+        logging.error(f'Exception when getting element')
 
 
 def get_ctrl_key(action):
-	try:
-		return action['ctrlKey']
-	except Keyerror as ke:
-		logging.error('Current action doesn\'t have a ctrlKey attribute')
+    try:
+        return action['ctrlKey']
+    except Keyerror as ke:
+        logging.error('Current action doesn\'t have a ctrlKey attribute')
 
 
 def get_key(action):
-	try:
-		return action['key']
-	except Keyerror as ke:
-		logging.error('Current action doesn\'t have a key attribute')
+    try:
+        return action['key']
+    except Keyerror as ke:
+        logging.error('Current action doesn\'t have a key attribute')
 
 
 def get_window_size(action):
-	try:
-		window_width = action['windowOuterWidth']
-		window_height = action['windowOuterHeight']
-		return window_width, window_height
-	except Keyerror as ke:
-		logging.error('error getting window size : action has no "windowInnerWidth" or "windowInnerHeight" field.')
+    try:
+        window_width = action['windowOuterWidth']
+        window_height = action['windowOuterHeight']
+        return window_width, window_height
+    except Keyerror as ke:
+        logging.error('error getting window size : action has no "windowInnerWidth" or "windowInnerHeight" field.')
 
 
 def get_keys_to_input(action):
-	try:
-		action_data = action["data"]
-	except Keyerror as ke:
-		logging.error('error getting keys to input : action has no "data" field.')
+    try:
+        action_data = action["data"]
+    except Keyerror as ke:
+        logging.error('error getting keys to input : action has no "data" field.')
 
-	return action_data
+    return action_data
 
 
 def get_scroll_params(action):
-	try:
+    try:
         return action['scrollX'], action['scrollY']
     except Exception as e:
         print('Exception getting the scrollX and scrollY property.')
@@ -106,32 +113,32 @@ def _get_element_xpath(element):
 def _get_action_id(action):
     try:
         action_target = action['target']
-	except Keyerror as ke:
-		logging.error('error getting action ID : action has no "target" field.')
-	else:
-		try:
-			action_id =  action_target["id"]
-		except Keyerror as ke:
-			logging.error('error getting action ID : action target has no "id" field.')
+    except Keyerror as ke:
+        logging.error('error getting action ID : action has no "target" field.')
+    else:
+        try:
+            action_id =  action_target["id"]
+        except Keyerror as ke:
+            logging.error('error getting action ID : action target has no "id" field.')
 
-	if action_id == None or action_id == "":
-		return None
+    if action_id == None or action_id == "":
+        return None
 
-	return action_id
+    return action_id
 
 
 def _get_action_css_selector(action):
-	try:
-		action_target = action['target']
-	except Keyerror as ke:
-		logging.error('error getting action css_selector : action has no "target" field.')
-	else:
-		try:
-			action_class_name =  action_target["className"]
-		except Keyerror as ke:
-			logging.error('error getting action css_selector : action target has no "className" field.')
+    try:
+        action_target = action['target']
+    except Keyerror as ke:
+        logging.error('error getting action css_selector : action has no "target" field.')
+    else:
+        try:
+            action_class_name =  action_target["className"]
+        except Keyerror as ke:
+            logging.error('error getting action css_selector : action target has no "className" field.')
 
-	if action_class_name == None or action_class_name == "":
-		return None
+    if action_class_name == None or action_class_name == "":
+        return None
 
-	return action_class_name
+    return action_class_name
